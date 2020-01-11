a0d007216b9dcd7b05ad4697e28c42eb5547144c
Bebleo <james.warne@outlook.com>
2020-01-10 21:37:53 -0500

    Remove relative imports

--- checklist_app/forms/__init__.py
+++ checklist_app/forms/__init__.py
@@ -1,5 +1,7 @@
 # flake8: noqa
-from .admin_forms import AddUserForm, EditUserForm
-from .auth_forms import (LoginForm, RegistrationForm, SendPasswordChangeForm,
-                         UpdatePasswordForm)
-from .checklist_forms import AddItemForm, CreateListForm, EditListForm
+from checklist_app.admin_forms import AddUserForm, EditUserForm
+from checklist_app.auth_forms import (LoginForm, RegistrationForm,
+                                      SendPasswordChangeForm,
+                                      UpdatePasswordForm)
+from checklist_app.checklist_forms import (AddItemForm, CreateListForm,
+                                           EditListForm)
