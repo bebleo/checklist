2de9846c9d18552de641dfdd61c4d32bb678f7da
Bebleo <james.warne@outlook.com>
2020-01-24 19:56:57 -0500

    Fix formatting on imports
    Cosmetic changes to fix the formatting for imports

--- checklist_app/forms/__init__.py
+++ checklist_app/forms/__init__.py
@@ -1,7 +1,5 @@
 # flake8: noqa
 from .admin_forms import AddUserForm, EditUserForm
-from .auth_forms import (LoginForm, RegistrationForm,
-                                      SendPasswordChangeForm,
-                                      UpdatePasswordForm)
-from .checklist_forms import (AddItemForm, CreateListForm,
-                                           EditListForm)
+from .auth_forms import (LoginForm, RegistrationForm, SendPasswordChangeForm,
+                         UpdatePasswordForm)
+from .checklist_forms import AddItemForm, CreateListForm, EditListForm
